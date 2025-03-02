a68d3ce37effde5a2d062dca04b7e7aa60177408
Austin Morrow <austinmorrowdev@gmail.com>
2025-03-01 17:40:59 -0700

    init repo

---
+++ src/app/expenses/page.js
@@ -0,0 +1,19 @@
+export default function Expenses() {
+    return (
+      <>
+        <div className="min-h-full">
+          <div className="py-10">
+            <header>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">Expenses</h1>
+              </div>
+            </header>
+            <main>
+              <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">{/* Your content */}</div>
+            </main>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
---
+++ src/app/signup/page.js
@@ -0,0 +1,142 @@
+import Link from "next/link";
+
+export default function SignUp() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            alt="Your Company"
+            src="/milli.svg"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Create an account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <form action="#" method="POST" className="space-y-6">
+              <div className="flex gap-4">
+                <div className="w-1/2">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    First name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="first-name"
+                      name="firstName"
+                      type="text"
+                      required
+                      autoComplete="given-name"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#00bf63] sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-1/2">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="last-name"
+                      name="lastName"
+                      type="text"
+                      required
+                      autoComplete="family-name"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#00bf63] sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#00bf63] sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#00bf63] sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Confirm password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#00bf63] sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div className="pt-2">
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-[#00bf63] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-[#33d98a] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#00bf63]"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              className="font-semibold text-[#00bf63] hover:text-[#33d98a]"
+            >
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
